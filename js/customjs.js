9d4872e6e64591793a443235c425b59d8cc0df85
Chris Mannel <cmannel77@gmail.com>
2016-02-20 15:19:31 -0600

    Fixed js
    back_to_top button and smooth scroll

--- js/customjs.js
+++ js/customjs.js
@@ -4,7 +4,7 @@ $('#mblmenu').click(function() {
   $('.mblnav').slideToggle();
 });
 
-// smooth scroll function for hash links 
+// smooth scroll function for hash links
 /*$(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -22,33 +22,33 @@ $('#mblmenu').click(function() {
 
 //nav-to-top arrow function
 $(document).ready(function() {
-var offset = 250;
-var duration = 300;
-$(window).scroll(function() {
-if (jQuery(this).scrollTop() > offset) {
-  jQuery('.back-to-top').fadeIn(duration); 
-  } else {
-    jQuery('.back-to-top').fadeOut(duration);
-  }
-});
-&nbsp;
-$(‘.back-to-top’).click(function(event) {
-  event.preventDefault();
-  $(‘html, body’).animate({scrollTop: 0}, duration);
-  return false;
-});
-
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
+  var offset = 250;
+  var duration = 300;
+  $(window).scroll(function() {
+  if (jQuery(this).scrollTop() > offset) {
+    jQuery('.back_to_top').fadeIn(duration);
+    } else {
+      jQuery('.back_to_top').fadeOut(duration);
     }
-  }
-})
- 
+  });
+
+  $('.back_to_top').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, duration);
+    return false;
+  });
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
