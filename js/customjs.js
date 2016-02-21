cac629f23afb569840e5b1a59b518f09bdee01c7
Chris Mannel <cmannel77@gmail.com>
2016-02-21 00:03:47 -0600

    further responsive updates
    Mobile and desktop nav fixes
    Font size changed to ‘vw’ (viewport) based in some instances

--- js/customjs.js
+++ js/customjs.js
@@ -1,28 +1,8 @@
 /* Pixel Engineering Co -- Custom js -- Handcrafted Code -- */
 
-$('#mblmenu').click(function() {
-  $('.mblnav').slideToggle();
-});
-
-// smooth scroll function for hash links
-/*$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});*/
-
-//nav-to-top arrow function
 $(document).ready(function() {
-  var offset = 250;
+  //nav-to-top arrow function
+  var offset = 400; 
   var duration = 300;
   $(window).scroll(function() {
   if (jQuery(this).scrollTop() > offset) {
@@ -37,7 +17,8 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop: 0}, duration);
     return false;
   });
-
+  
+  //smooth scroll fuction for hash links
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       var target = $(this.hash);
@@ -49,6 +30,10 @@ $(document).ready(function() {
         return false;
       }
     }
+  });
+  
+  $('#mblmenu').click(function() {
+    $('.mblnav').slideToggle();
   });
 
 });
